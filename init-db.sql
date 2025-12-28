1c34f3311ade66f42161bb9d1d102052439e8569
marga1978 <marco.galante78@gmail.com>
2025-12-28 18:21:26 +0100

    update config file

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,6 @@
 -- Script di inizializzazione del database
-CREATE DATABASE IF NOT EXISTS students_courses_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_lms CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE students_courses_db;
+USE db_lms;
 
 -- Il resto delle tabelle verrà creato automaticamente da TypeORM con synchronize: true
